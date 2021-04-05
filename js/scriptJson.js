f7227b8653ea2f5d90537d95e694f346455df048
andreamorey <andreamoreysanchez@gmail.com>
2021-04-05 18:41:32 +0200

    json entero + pruebas js

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -1,97 +1,41 @@
-/*!
-    * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-(function ($) {
-  "use strict"; // Start of use strict
+var request = new XMLHttpRequest();
+var url = "js/datos.json";
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 71)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+request.open("GET", url, true);
+request.responseType = 'text';
+request.send();
 
-  // Scroll to top button appear
-  $(document).scroll(function () {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+request.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    const deportesText = request.response; //cogemos la cadena de response
+    const deportes = JSON.parse(deportesText); //la convertimos a objeto
+    showSports(deportes);
+  }
+}
+function showSports(d) {
+  const sports = d['dadesPropies']['esports'];
+  for (var i = 0; i < sports.length; i++) {
+    var a = document.createElement('div');
+    a.className = 'col-md-6 col-lg-4 mb-5';
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
-  });
+    var b = document.createElement('div');
+    b.className = 'portfolio-item mx-auto';
+    b.attr("data-toggle", "modal");
+    b.attr("data-target", "#portfolioModal1");
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 80
-  });
+    var img = document.createElement('img');
+    img.src = d['imatges'][i];
+    b.appendChild(img);
+    a.appendChild(b);
 
-  // Collapse Navbar
-  var navbarCollapse = function () {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+    var c = document.createElement('div');
+    c.className = 'portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100';
+    b.appendChild(c);
 
-  // Floating label headings for the contact form
-  $(function () {
-    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function () {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function () {
-      $(this).removeClass("floating-label-form-group-with-focus");
-    });
-  });
-
-
-  //Carousel
-  $(document).ready(function () {
-    // Activate Carousel
-    $("#myCarousel").carousel();
-
-    // Enable Carousel Indicators
-    $(".item1").click(function () {
-      $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function () {
-      $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function () {
-      $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function () {
-      $("#myCarousel").carousel(3);
-    });
-
-    // Enable Carousel Controls
-    $(".left").click(function () {
-      $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function () {
-      $("#myCarousel").carousel("next");
-    });
-  });
-
-})(jQuery); // End of use strict
+    var d = document.createElement('div');
+    d.className = 'portfolio-item-caption-content text-center text-white sport';
+    d.textContent = sports[i];
+    c.appendChild(d);
+  }
+  document.getElementById("catalogo").appendChild(a);
+}
