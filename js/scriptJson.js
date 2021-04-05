9dc7a65ad73bd5fdff506ef026854f7c78849fb6
andreamorey <andreamoreysanchez@gmail.com>
2021-04-05 20:14:43 +0200

    he cambiado lo de las clases css de los div

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -18,29 +18,28 @@ function showSports(d) {
   
   const sports = d['dadesPropies']['esports'];
   for (var i = 0; i < sports.length; i++) {
-    /*
+    
     var a = document.createElement("div");
-    a.className = 'col-md-6 col-lg-4 mb-5';
+    a.classList.add('col-md-6', 'col-lg-4', 'mb-5');
 
     var b = document.createElement("div");
-    b.className = 'portfolio-item mx-auto';
+    b.classList.add('portfolio-item', 'mx-auto');
     b.attr("data-toggle", "modal");
     b.attr("data-target", "#portfolioModal1");
-    */
+    
     var img = document.createElement("img");
     img.src = d['imatges'][i];
-    //b.appendChild(img);
-    catalogo.appendChild(img);
-    //a.appendChild(b);
-    /*
+    b.appendChild(img);
+    a.appendChild(b);
+    
     var c = document.createElement("div");
-    c.className = 'portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100';
-    //b.appendChild(c);
+    c.classList.add('portfolio-item-caption', 'd-flex', 'align-items-center', 'justify-content-center', 'h-100', 'w-100');
+    b.appendChild(c);
 
     var d = document.createElement("div");
-    d.className = 'portfolio-item-caption-content text-center text-white sport';
+    d.classList.add('portfolio-item-caption-content', 'text-center', 'text-white', 'sport');
     d.innerHTML = sports[i];
-    //c.appendChild(d);
+    c.appendChild(d);
 
     catalogo.appendChild(a);
     /*
