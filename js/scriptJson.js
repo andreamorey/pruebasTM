7723229db2018355de15c744b47138a9ee63d32b
andreamorey <andreamoreysanchez@gmail.com>
2021-04-05 19:52:31 +0200

    cambio llaves json

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -16,7 +16,7 @@ request.onreadystatechange = function () {
 
 function showSports(d) {
   
-  const sports = d['dadesPropies'][0];
+  const sports = d['dadesPropies']['esports'];
   for (var i = 0; i < sports.length; i++) {
     /*
     var a = document.createElement("div");
