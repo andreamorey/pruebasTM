e3c6d2892904b87828a9b5130bf807f74ba1802d
andreamorey <andreamoreysanchez@gmail.com>
2021-04-05 19:07:33 +0200

    prueba 2

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -1,3 +1,4 @@
+const catalogo = document.getElementById('catalogo');
 var request = new XMLHttpRequest();
 var url = "js/datos.json";
 
@@ -12,31 +13,33 @@ request.onreadystatechange = function () {
     showSports(deportes);
   }
 }
+
 function showSports(d) {
   const sports = d['dadesPropies']['esports'];
   for (var i = 0; i < sports.length; i++) {
-    var a = document.createElement('div');
+    var a = document.createElement("div");
     a.className = 'col-md-6 col-lg-4 mb-5';
 
-    var b = document.createElement('div');
+    var b = document.createElement("div");
     b.className = 'portfolio-item mx-auto';
     b.attr("data-toggle", "modal");
     b.attr("data-target", "#portfolioModal1");
 
-    var img = document.createElement('img');
+    var img = document.createElement("img");
     img.src = d['imatges'][i];
     b.appendChild(img);
     a.appendChild(b);
 
-    var c = document.createElement('div');
+    var c = document.createElement("div");
     c.className = 'portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100';
     b.appendChild(c);
 
-    var d = document.createElement('div');
+    var d = document.createElement("div");
     d.className = 'portfolio-item-caption-content text-center text-white sport';
     d.textContent = sports[i];
     c.appendChild(d);
+
+    catalogo.appendChild(a);
   }
-  document.getElementById("catalogo").appendChild(a);
-  document.getElementById("catalogo").innerHTML("putooooooo");
+  catalogo.innerHTML('<br>Putooooooooo!');
 }
